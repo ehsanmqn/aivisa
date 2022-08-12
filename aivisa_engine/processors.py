2bdae65982887008c2a5a8481ff0b1eeac8844b9
Ehsan <spin313@gmail.com>
2022-08-12 21:52:34 +0430

    rembag parameters updated for better result

--- aivisa_engine/processors.py
+++ aivisa_engine/processors.py
@@ -52,10 +52,10 @@ def remove_background(data):
     session = new_session("u2net_human_seg")
 
     output = remove(data=data,
-                    alpha_matting=True,
+                    # alpha_matting=True,
                     # alpha_matting_foreground_threshold=300,
-                    # alpha_matting_background_threshold=20,
-                    alpha_matting_erode_size=5,
+                    # alpha_matting_background_threshold=0,
+                    # alpha_matting_erode_size=0,
                     # only_mask=True,
                     post_process_mask=True,
                     session=session
